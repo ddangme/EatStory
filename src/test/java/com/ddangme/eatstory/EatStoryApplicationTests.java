b158e61ae43904146f8d1a9f8e17e9ef5eb404c7
ddangme <ssilver121@naver.com>
2024-03-19 17:08:00 +0900

    chore: init project
    프로젝트 초기 세팅

---
+++ src/test/java/com/ddangme/eatstory/EatStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddangme.eatstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatStoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
