6b2b110b24ec3e22671d19b6dd9d610f52cc2a04
ddangme <ssilver121@naver.com>
2024-03-22 05:01:20 +0900

    chore: JWT 인증 관련 의존성 제외

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (user_status, created_at, id, modified_at, created_by, modified_by, img, nickname, password, user_id, user_role)
+    VALUES ('NORMAL', now(), 1, now(), 'ddangme', now(), null, 'ddang_me', '{noop}qwer1234', 'ddang_me', 'USER');
