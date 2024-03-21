c58ffd7b5b054d51699d3d67c6c626b6cccad1c9
ddangme <ssilver121@naver.com>
2024-03-22 08:24:02 +0900

    fix: 회원가입 프론트 문구 수정

--- src/main/java/com/ddangme/eatstory/dto/request/UserJoinRequest.java
+++ src/main/java/com/ddangme/eatstory/dto/request/UserJoinRequest.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Data
@@ -15,7 +14,7 @@ public class UserJoinRequest {
     @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리로 입력해주세요.")
     private String userId;
 
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문, 숫자, 특수문자 조합으로 입력해주세요.")
     private String password;
 
     private String passwordCheck;
